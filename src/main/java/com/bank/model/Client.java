03a664e553c9be8069819d1b50025daa6dd84068
m3ntor <alive.exe@gmail.com>
2026-04-22 15:24:10 +0300

    feat: create basic Client class

--- src/main/java/com/bank/model/Client.java
+++ src/main/java/com/bank/model/Client.java
@@ -1,4 +1,50 @@
 package com.bank.model;
 
 public class Client {
+    private String firstName;
+    private String secondName;
+    private String email;
+    private String phoneNumber;
+
+    public Client(String firstName,
+                  String secondName,
+                  String email,
+                  String phoneNumber) {
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
