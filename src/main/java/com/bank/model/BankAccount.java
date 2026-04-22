bc4299cf4af4aead829403d267795b96a2d14c90
m3ntor <alive.exe@gmail.com>
2026-04-22 15:28:29 +0300

    feat: create basic BankAccount class

--- src/main/java/com/bank/model/BankAccount.java
+++ src/main/java/com/bank/model/BankAccount.java
@@ -1,4 +1,37 @@
 package com.bank.model;
 
 public class BankAccount {
+    private String accountNumber;
+    private Currency currency;
+    private int balance;
+
+    public BankAccount(String accountNumber, Currency currency, int balance) {
+        this.accountNumber = accountNumber;
+        this.currency = currency;
+        this.balance = balance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public Currency getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(Currency currency) {
+        this.currency = currency;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
 }
