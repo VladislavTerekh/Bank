c01a683c036e1f737efd955685efce7f111737b7
m3ntor <alive.exe@gmail.com>
2026-04-26 18:37:53 +0300

    upd: add comment for educational purposes

--- src/main/java/com/bank/exception/UserEmailAlreadyInUseException.java
+++ src/main/java/com/bank/exception/UserEmailAlreadyInUseException.java
@@ -1,5 +1,6 @@
 package com.bank.exception;
 
+//extend runtimeE to make it unchecked, extend Exception to make in checked
 public class UserEmailAlreadyInUseException extends RuntimeException {
     public UserEmailAlreadyInUseException(String message) {
         super(message);
