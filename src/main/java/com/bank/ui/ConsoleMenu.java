f2713c0765e9cac025169ee7495bbe9ce2b1a26c
m3ntor <alive.exe@gmail.com>
2026-05-12 14:19:14 +0300

    feat: create menu's and implement inputHelper's

--- src/main/java/com/bank/ui/ConsoleMenu.java
+++ src/main/java/com/bank/ui/ConsoleMenu.java
@@ -1,21 +1,21 @@
 package com.bank.ui;
 
 import com.bank.service.ClientService;
+import com.bank.ui.helper.InputHelper;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.SQLOutput;
+
 import java.util.Scanner;
 
 public class ConsoleMenu {
 
-    private ClientService clientService;
-    private Scanner scanner;
+    private final ClientService clientService;
+    private final Scanner scanner;
+    private final InputHelper inputHelper;
 
     public ConsoleMenu(ClientService clientService) {
         this.clientService = clientService;
         this.scanner = new Scanner(System.in);
+        this.inputHelper = new InputHelper(scanner);
     }
 
     public void start() {
@@ -24,12 +24,11 @@ public class ConsoleMenu {
             System.out.println("1. Client Management");
             System.out.println("0. Exit");
 
-            int choice = scanner.nextInt();
-            scanner.nextLine();
+            int choice = inputHelper.getIntInput();
 
             switch (choice) {
                 case 1:
-                    //call a new menu sheet
+                    clientManagementMenu();
                     break;
                 case 0:
                     System.out.println("Goodbye!");
@@ -39,16 +38,57 @@ public class ConsoleMenu {
             }
 
 
-            if (true) {
-               return;
-            }
-
         }
 
     }
 
     private void clientManagementMenu() {
-        // TODO: CRUD menu
+        while (true) {
+            System.out.println("\n=== CLIENT MANAGEMENT MENU ===");
+            System.out.println("1. Create client");
+            System.out.println("2. Create bank account for client");
+            System.out.println("3. Get client info");
+            System.out.println("4. Add funds");
+            System.out.println("5. Withdraw funds");
+            System.out.println("6. Get list of all clients");
+            System.out.println("0. Back");
+
+            int choice = inputHelper.getIntInput();
+
+            if (choice == 0) {
+                break;
+            }
+
+            ClientManagementMenu cmm = new ClientManagementMenu(scanner, clientService, inputHelper);
+
+            switch (choice) {
+                case 1:
+                    // Call createClient()
+                    break;
+                case 2:
+                    // Call createBankAccount()
+                    break;
+                case 3:
+                    // Call getClientInfo()
+                    break;
+                case 4:
+                    // Call addFunds()
+                    break;
+                case 5:
+                    // Call withdrawFunds()
+                    break;
+                case 6:
+                    // Call GetListOfAllClients()
+                    break;
+                default:
+                    System.out.println("Invalid choice!");
+            }
+
+
+
+
+        }
+
     }
 
 }
