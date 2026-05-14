e74e3233ace5f509d065a19809943ee00c343f3f
m3ntor <alive.exe@gmail.com>
2026-05-14 14:00:26 +0300

    upd: update createClient() methods and provide checks for duplicate Email and PhoneNumber

--- src/main/java/com/bank/ui/ClientManagementMenu.java
+++ src/main/java/com/bank/ui/ClientManagementMenu.java
@@ -1,7 +1,11 @@
 package com.bank.ui;
 
+import com.bank.exception.UserEmailAlreadyInUseException;
+import com.bank.exception.UserPhoneNumberAlreadyInUseException;
 import com.bank.service.ClientService;
 import com.bank.ui.helper.InputHelper;
+
+import java.sql.SQLException;
 
 public class ClientManagementMenu {
     private final ClientService clientService;
@@ -13,20 +17,61 @@ public class ClientManagementMenu {
     }
 
     public void createClient() {
-        String firstName = inputHelper.getNonEmptyString("To create a new client, enter name / enter 0 to abort whole operation: ");
+        String additionalMsg = " / enter 0 to abort whole operation: ";
+
+        String firstName = inputHelper.getNonEmptyString("To create a new client, enter name" + additionalMsg);
         if (firstName.equals("0")) return;
-        String secondName = inputHelper.getNonEmptyString("Enter name / enter 0 to abort whole operation: ");
+
+        String secondName = inputHelper.getNonEmptyString("Enter second name" + additionalMsg);
         if (secondName.equals("0")) return;
 
-        // TODO: email check from DB (if exists) implemented with while cycle
-        String email = inputHelper.getEmailAddress("Enter email / enter 0 to abort whole operation: ");
-        if (email.equals("0")) return;
 
-        // TODO: phoneNumber check from DB (if exists) implemented with while cycle
-        String phoneNumber = inputHelper.getNonEmptyString("Enter phone number / enter 0 to abort whole operation: ");
-        if (phoneNumber.equals("0")) return;
+        String email;
+        while (true) {
+            email = inputHelper.getEmailAddress("Enter email" + additionalMsg);
+            if (email.equals("0")) return;
 
-        clientService.createClient(firstName, secondName, email, phoneNumber);
+            try {
+                if (!clientService.isEmailExists(email)) {
+                    return;
+                } else {
+                    System.out.println("Email is already in use, try another one");
+                    break;
+                }
+            } catch (SQLException e) {
+                System.out.println("Couldn't check email existence, try again later.");
+            }
+        }
+
+
+
+        String phoneNumber;
+        while (true) {
+            phoneNumber = inputHelper.getNonEmptyString("Enter phone number" + additionalMsg);
+            if (phoneNumber.equals("0")) return;
+
+            try {
+                if (!clientService.isPhoneExists(phoneNumber)) {
+                    return;
+                } else {
+                    System.out.println("Phone is already in use, try another one");
+                    break;
+                }
+            } catch (SQLException e) {
+                System.out.println("Couldn't check phone existence, try again later.");
+            }
+
+        }
+
+        try {
+            clientService.createClient(firstName, secondName, email, phoneNumber);
+        } catch (SQLException e) {
+            System.out.println(" ");
+        } catch (UserEmailAlreadyInUseException e) {
+            System.out.println("  ");
+        } catch (UserPhoneNumberAlreadyInUseException e) {
+            System.out.println("   ");
+        }
 
         System.out.println("Returning to a previous menu page");
     }
