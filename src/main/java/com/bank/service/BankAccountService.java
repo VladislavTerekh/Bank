1f103208d763a4263c69666c4d01da4b29839e04
m3ntor <alive.exe@gmail.com>
2026-05-15 16:05:04 +0300

    feat: extend BankAccountService

--- src/main/java/com/bank/service/BankAccountService.java
+++ src/main/java/com/bank/service/BankAccountService.java
@@ -1,4 +1,17 @@
 package com.bank.service;
 
+import com.bank.dao.BankAccountDao;
+import com.bank.model.Currency;
+
 public class BankAccountService {
+
+    private BankAccountDao bankAccountDao;
+
+    public BankAccountService(BankAccountDao bankAccountDao) {
+        this.bankAccountDao = bankAccountDao;
+    }
+
+    public void createAccount(String clientId, Currency currency) {
+        bankAccountDao.createAccount(clientId, currency);
+    }
 }
