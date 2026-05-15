691e25dd09e7688c217ae9cd98332b021a16950a
m3ntor <alive.exe@gmail.com>
2026-05-15 16:11:33 +0300

    upd: add bankAccountService and Transaction service integration

--- src/main/java/com/bank/main/Main.java
+++ src/main/java/com/bank/main/Main.java
@@ -1,7 +1,11 @@
 package com.bank.main;
 
+import com.bank.dao.BankAccountDao;
 import com.bank.dao.ClientDao;
+import com.bank.dao.TransactionDao;
+import com.bank.service.BankAccountService;
 import com.bank.service.ClientService;
+import com.bank.service.TransactionService;
 import com.bank.ui.ConsoleMenu;
 
 import java.sql.Connection;
@@ -10,15 +14,25 @@ import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
-        String url = "jdbc:mysql://localhost:3306/Bank";
-        String user = "root";
-        String password = "9876543210";
+        String url = "jdbc:mysql://localhost:3306/Bank"; //security issue
+        String user = "root"; //security issue
+        String password = "9876543210"; //security issue
 
         try (Connection connection = DriverManager.getConnection(url, user, password)){
 
             ClientDao clientDao = new ClientDao(connection);
             ClientService clientService = new ClientService(clientDao);
-            ConsoleMenu ui = new ConsoleMenu(clientService);
+
+            BankAccountDao bankAccountDao = new BankAccountDao(connection);
+            BankAccountService bankAccountService = new BankAccountService(bankAccountDao);
+
+            TransactionDao transactionDao = new TransactionDao(connection);
+            TransactionService transactionService = new TransactionService(transactionDao);
+
+
+            ConsoleMenu ui = new ConsoleMenu(clientService, bankAccountService, transactionService);
+
+
             System.out.println("Connection successful.");
             ui.start();
 
