3f35811aad19b5f9527a8bf64363ebb26c06aebe
m3ntor <alive.exe@gmail.com>
2026-05-15 16:04:52 +0300

    feat: extend BankAccountDao

--- src/main/java/com/bank/dao/BankAccountDao.java
+++ src/main/java/com/bank/dao/BankAccountDao.java
@@ -1,4 +1,18 @@
 package com.bank.dao;
 
+import com.bank.model.Currency;
+
+import java.sql.Connection;
+
 public class BankAccountDao {
+
+    Connection connection;
+
+    public BankAccountDao(Connection connection) {
+        this.connection = connection;
+    }
+
+    public void createAccount(String clientId, Currency currency) {
+        //TODO: implement method
+    }
 }
