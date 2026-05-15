e6c0d744b188ea00ed06d35a09afd670c4579bc7
m3ntor <alive.exe@gmail.com>
2026-05-15 16:12:18 +0300

    upd: small updates to Transaction Dao and Service classes

--- src/main/java/com/bank/dao/TransactionDao.java
+++ src/main/java/com/bank/dao/TransactionDao.java
@@ -1,4 +1,12 @@
 package com.bank.dao;
 
+import java.sql.Connection;
+
 public class TransactionDao {
+
+    Connection connection;
+
+    public TransactionDao(Connection connection) {
+        this.connection = connection;
+    }
 }
--- src/main/java/com/bank/service/TransactionService.java
+++ src/main/java/com/bank/service/TransactionService.java
@@ -1,4 +1,14 @@
 package com.bank.service;
 
+import com.bank.dao.TransactionDao;
+
 public class TransactionService {
+
+    private TransactionDao transactionDao;
+
+    public TransactionService(TransactionDao transactionDao) {
+        this.transactionDao = transactionDao;
+    }
+
+
 }
